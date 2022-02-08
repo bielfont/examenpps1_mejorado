b5777a3aa5989e14ba69dcafe4afceb0f8dc07d5
Biel Font <biel.font@inphower.com>
2022-02-08 12:59:29 +0100

    4

--- test_.py
+++ test_.py
@@ -5,5 +5,6 @@ import datos
 def test1():
     assert alumnes.orden_nombre(datos.students) == str(datos.students_orden_nom)
 
+
 def test2():
     assert alumnes.orden_data(datos.students) == str(datos.students_orden_data)
